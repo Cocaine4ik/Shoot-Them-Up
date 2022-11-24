05f23d4f1fd96b6cd44dac0091ff1489e9553590
Cocaine <kokotkin.alexander@gmail.com>
2022-11-24 22:59:24 +0200

    Add Make damage function

--- Source/ShootThemUp/Public/Weapons/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapons/STUBaseWeapon.h
@@ -20,6 +20,9 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USkeletalMeshComponent* WeaponMesh;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Damage")
+    float DamageAmount = 10.0f;
+
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
     FName MuzzleSocketName = "MuzzleSocket";
 
@@ -29,10 +32,10 @@ protected:
     virtual void BeginPlay() override;
 
     void MakeShot();
-    
     APlayerController* GetPlayerController() const;
     bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
     FVector GetMuzzleWorldLocation() const;
     bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const;
     void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd);
+    void MakeDamage(const FHitResult HitResult);
 };
