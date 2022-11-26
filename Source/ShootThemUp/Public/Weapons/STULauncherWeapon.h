cd8444dab59ef4d8dd6da977cf00630688679b15
Cocaine <kokotkin.alexander@gmail.com>
2022-11-26 21:33:59 +0200

    Update STULauncherWeapon.h

--- Source/ShootThemUp/Public/Weapons/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapons/STULauncherWeapon.h
@@ -3,15 +3,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "STUProjectile.h"
 #include "Weapons/STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    ASTULauncherWeapon();
+    virtual void StartFire() override;
+    
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Damage")
+    float DamageRadius = 200.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Damage")
+    bool bDoFullDamage = false;
+    
+    virtual void MakeShot() override;
 };
