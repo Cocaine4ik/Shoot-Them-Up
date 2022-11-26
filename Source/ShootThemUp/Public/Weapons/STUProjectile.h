0ec4203e7e797aaf0eb89d85a25388a0a656fff7
Cocaine <kokotkin.alexander@gmail.com>
2022-11-26 21:34:01 +0200

    Update STUProjectile.h

--- Source/ShootThemUp/Public/Weapons/STUProjectile.h
+++ Source/ShootThemUp/Public/Weapons/STUProjectile.h
@@ -3,20 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "STUProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUProjectile : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ASTUProjectile();
+    GENERATED_BODY()
+
+public:
+    ASTUProjectile();
+
+    void SetShotDirection(const FVector& Direction) { ShotDirection = Direction; }
+    void SetDamageAmount(const float Damage) { DamageAmount = Damage; }
+    void SetDamageRadius(const float Radius) { DamageRadius = Radius; }
+    void SetDoFullDamage(const bool DoFullDamage) { bDoFullDamage = DoFullDamage; }
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    UPROPERTY(VisibleDefaultsOnly, Category = "Weapon")
+    USphereComponent* CollisionComponent;
+
+    UPROPERTY(VisibleDefaultsOnly, Category = "Weapon")
+    UProjectileMovementComponent* MovementComponent;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+    float LifeSeconds = 5.0f;
     
+    virtual void BeginPlay() override;
+
+private:
+    FVector ShotDirection;
+    float DamageAmount;
+    float DamageRadius;
+    bool bDoFullDamage;
+    
+    UFUNCTION()
+    void OnProjectileHit(UPrimitiveComponent*HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+    AController* GetController() const;
 };
