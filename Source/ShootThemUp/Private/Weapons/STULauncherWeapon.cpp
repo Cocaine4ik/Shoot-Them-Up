5e84414ea72144a2f430cc958cca571abc29d906
Cocaine <kokotkin.alexander@gmail.com>
2022-11-26 21:33:43 +0200

    Update STULauncherWeapon.cpp

--- Source/ShootThemUp/Private/Weapons/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapons/STULauncherWeapon.cpp
@@ -2,4 +2,42 @@
 
 
 #include "Weapons/STULauncherWeapon.h"
+#include "Weapons/STUProjectile.h"
+
+ASTULauncherWeapon::ASTULauncherWeapon():ASTUBaseWeapon()
+{
+    DamageAmount = 50.0f;
+}
+
+void ASTULauncherWeapon::StartFire()
+{
+    MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+    if (!GetWorld()) return;
+
+    FVector TraceStart, TraceEnd;
+    if(!GetTraceData(TraceStart, TraceEnd)) return;
+    
+    FHitResult HitResult;
+    MakeHit(HitResult, TraceStart, TraceEnd);
+
+    const FVector EndPoint = HitResult.bBlockingHit ? HitResult.ImpactPoint : TraceEnd;
+    const FVector Direction = (EndPoint - GetMuzzleWorldLocation()).GetSafeNormal();
+    
+    const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+    ASTUProjectile* Projectile = GetWorld()->SpawnActorDeferred<ASTUProjectile>(ProjectileClass, SpawnTransform);
+    if(Projectile)
+    {
+        Projectile->SetShotDirection(Direction);
+        Projectile->SetDamageAmount(DamageAmount);
+        Projectile->SetDamageRadius(DamageRadius);
+        Projectile->SetDoFullDamage(bDoFullDamage);
+        Projectile->SetOwner(GetOwner());
+        Projectile->FinishSpawning(SpawnTransform);
+    }
+    // set projectile params
+}
 
