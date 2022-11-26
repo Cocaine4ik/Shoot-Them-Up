2a79bf8bf2ad1bfe1b71d7d0dd774b1e42cad8c7
Cocaine <kokotkin.alexander@gmail.com>
2022-11-26 18:48:16 +0200

    Create STUProjectile.cpp

---
+++ Source/ShootThemUp/Private/Weapons/STUProjectile.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/STUProjectile.h"
+
+ASTUProjectile::ASTUProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASTUProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
