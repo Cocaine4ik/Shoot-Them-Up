8c66fae06a082d0c0c54e356627266023ed8fa9a
Cocaine <kokotkin.alexander@gmail.com>
2022-12-15 01:04:00 +0200

    Add Base Bullet Impact Decals

--- Source/ShootThemUp/Private/Weapons/Components/STUWeaponFXComponent.cpp
+++ Source/ShootThemUp/Private/Weapons/Components/STUWeaponFXComponent.cpp
@@ -5,6 +5,8 @@
 
 #include "NiagaraFunctionLibrary.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/DecalComponent.h"
 
 USTUWeaponFXComponent::USTUWeaponFXComponent()
 {
@@ -13,15 +15,31 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactEffects(const FHitResult& Hit)
 {
-    auto Effect = DefaultEffect;
+    auto ImpactData = DefaultImpactData;
 
     if(Hit.PhysMaterial.IsValid())
     {
         const auto PhysMat = Hit.PhysMaterial.Get();
-        if(EffectsMap.Contains(PhysMat))
+        if(ImpactDataMap.Contains(PhysMat))
         {
-            Effect = EffectsMap[PhysMat];
+            ImpactData = ImpactDataMap[PhysMat];
         }
     }
-    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+    // spawn niagara
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), //
+        ImpactData.NiagaraEffect,                              //
+        Hit.ImpactPoint,                                       //
+        Hit.ImpactNormal.Rotation());
+
+    // spawn decal
+    auto DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(),
+        ImpactData.DecalData.Material, //
+        ImpactData.DecalData.Size,     //
+        Hit.ImpactPoint,               //
+        Hit.ImpactNormal.Rotation());
+
+    if(DecalComponent)
+    {
+        DecalComponent->SetFadeOut(ImpactData.DecalData.LifeTime, ImpactData.DecalData.FadeOutTime);
+    }
 }
